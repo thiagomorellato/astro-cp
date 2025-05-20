e9923efec9ea31563c003f403999b3d1413f177d
Thiago Morellato <thiagomorellatoa@gmail.com>
2025-05-20 19:23:40 -0300

    user panel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
     public function index()
     {
         if (!session()->has('astrocp_user')) {
-            return redirect()->route('account')->with('error', 'You must be logged in.');
+            return redirect()->route('astrocp.login.form')->with('error', 'You must be logged in.');
         }
 
         $user = session('astrocp_user');
